a9c47cec1fc3bd705da873483b407b0e3560270b
febriindra14 <febriindravalentino@gmail.com>
2022-11-15 03:22:10 +0700

    update template admin

---
+++ app/Views/admin/dashboard/index.php
@@ -0,0 +1,202 @@
+<ul class="display-none" id="default-search-main">
+    <li class="auto-suggestion-title"><a class="collection-item" href="#">
+            <h6 class="search-title">FILES</h6>
+        </a></li>
+    <li class="auto-suggestion"><a class="collection-item" href="#">
+            <div class="display-flex">
+                <div class="display-flex align-item-center flex-grow-1">
+                    <div class="avatar"><img src="../../../app-assets/images/icon/pdf-image.png" width="24" height="30" alt="sample image"></div>
+                    <div class="member-info display-flex flex-column"><span class="black-text">Two new item submitted</span><small class="grey-text">Marketing Manager</small></div>
+                </div>
+                <div class="status"><small class="grey-text">17kb</small></div>
+            </div>
+        </a></li>
+    <li class="auto-suggestion"><a class="collection-item" href="#">
+            <div class="display-flex">
+                <div class="display-flex align-item-center flex-grow-1">
+                    <div class="avatar"><img src="../../../app-assets/images/icon/doc-image.png" width="24" height="30" alt="sample image"></div>
+                    <div class="member-info display-flex flex-column"><span class="black-text">52 Doc file Generator</span><small class="grey-text">FontEnd Developer</small></div>
+                </div>
+                <div class="status"><small class="grey-text">550kb</small></div>
+            </div>
+        </a></li>
+    <li class="auto-suggestion"><a class="collection-item" href="#">
+            <div class="display-flex">
+                <div class="display-flex align-item-center flex-grow-1">
+                    <div class="avatar"><img src="../../../app-assets/images/icon/xls-image.png" width="24" height="30" alt="sample image"></div>
+                    <div class="member-info display-flex flex-column"><span class="black-text">25 Xls File Uploaded</span><small class="grey-text">Digital Marketing Manager</small></div>
+                </div>
+                <div class="status"><small class="grey-text">20kb</small></div>
+            </div>
+        </a></li>
+    <li class="auto-suggestion"><a class="collection-item" href="#">
+            <div class="display-flex">
+                <div class="display-flex align-item-center flex-grow-1">
+                    <div class="avatar"><img src="../../../app-assets/images/icon/jpg-image.png" width="24" height="30" alt="sample image"></div>
+                    <div class="member-info display-flex flex-column"><span class="black-text">Anna Strong</span><small class="grey-text">Web Designer</small></div>
+                </div>
+                <div class="status"><small class="grey-text">37kb</small></div>
+            </div>
+        </a></li>
+    <li class="auto-suggestion-title"><a class="collection-item" href="#">
+            <h6 class="search-title">MEMBERS</h6>
+        </a></li>
+    <li class="auto-suggestion"><a class="collection-item" href="#">
+            <div class="display-flex">
+                <div class="display-flex align-item-center flex-grow-1">
+                    <div class="avatar"><img class="circle" src="../../../app-assets/images/avatar/avatar-7.png" width="30" alt="sample image"></div>
+                    <div class="member-info display-flex flex-column"><span class="black-text">John Doe</span><small class="grey-text">UI designer</small></div>
+                </div>
+            </div>
+        </a></li>
+    <li class="auto-suggestion"><a class="collection-item" href="#">
+            <div class="display-flex">
+                <div class="display-flex align-item-center flex-grow-1">
+                    <div class="avatar"><img class="circle" src="../../../app-assets/images/avatar/avatar-8.png" width="30" alt="sample image"></div>
+                    <div class="member-info display-flex flex-column"><span class="black-text">Michal Clark</span><small class="grey-text">FontEnd Developer</small></div>
+                </div>
+            </div>
+        </a></li>
+    <li class="auto-suggestion"><a class="collection-item" href="#">
+            <div class="display-flex">
+                <div class="display-flex align-item-center flex-grow-1">
+                    <div class="avatar"><img class="circle" src="../../../app-assets/images/avatar/avatar-10.png" width="30" alt="sample image"></div>
+                    <div class="member-info display-flex flex-column"><span class="black-text">Milena Gibson</span><small class="grey-text">Digital Marketing</small></div>
+                </div>
+            </div>
+        </a></li>
+    <li class="auto-suggestion"><a class="collection-item" href="#">
+            <div class="display-flex">
+                <div class="display-flex align-item-center flex-grow-1">
+                    <div class="avatar"><img class="circle" src="../../../app-assets/images/avatar/avatar-12.png" width="30" alt="sample image"></div>
+                    <div class="member-info display-flex flex-column"><span class="black-text">Anna Strong</span><small class="grey-text">Web Designer</small></div>
+                </div>
+            </div>
+        </a></li>
+</ul>
+
+<ul class="display-none" id="page-search-title">
+    <li class="auto-suggestion-title"><a class="collection-item" href="#">
+            <h6 class="search-title">PAGES</h6>
+        </a></li>
+</ul>
+<ul class="display-none" id="search-not-found">
+    <li class="auto-suggestion"><a class="collection-item display-flex align-items-center" href="#"><span class="material-icons">error_outline</span><span class="member-info">No results found.</span></a></li>
+</ul>
+
+
+<!-- BEGIN: Page Main-->
+<div id="main">
+    <div class="row">
+        <div class="content-wrapper-before gradient-45deg-indigo-purple"></div>
+        <div class="breadcrumbs-dark pb-0 pt-4" id="breadcrumbs-wrapper">
+
+            <!-- Search for small screen-->
+            <div class="container">
+                <div class="row">
+                    <div class="col s10 m6 l6">
+                        <h5 class="breadcrumbs-title mt-0 mb-0"><span>Carousel</span></h5>
+                        <ol class="breadcrumbs mb-0">
+                            <li class="breadcrumb-item"><a href="index.html">Home</a>
+                            </li>
+                            <li class="breadcrumb-item"><a href="#">Advance UI</a>
+                            </li>
+                            <li class="breadcrumb-item active">Carousel
+                            </li>
+                        </ol>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+        <div class="col s12">
+            <div class="container">
+                <div class="section">
+
+                    <div class="card">
+                        <div class="card-content">
+                            <p class="caption">Our Carousel is a robust and versatile component that can be an image slider, to an item
+                                carousel, to an onboarding experience. It is touch enabled making it especially smooth to use on mobile.</p>
+                            <p>Note: This is also touch compatible! Try swiping with your finger to scroll through the carousel.</p>
+                        </div>
+                    </div>
+
+                    <!--Prefilling Text Inputs-->
+                    <div class="row">
+                        <div class="col s12">
+                            <div id="carousel" class="card card-tabs">
+                                <div class="card-content">
+                                    <div class="card-title">
+                                        <div class="row">
+                                            <div class="col s12 m6 l6">
+                                                <h4 class="card-title">Carousel</h4>
+                                            </div>
+                                            <div class="col s12 m6 l6">
+                                                <ul class="tabs">
+                                                    <li class="tab col s4 p-0"><a class="active p-0" href="#view-carousel">View</a></li>
+                                                    <li class="tab col s4 p-0"><a class="p-0" href="#html-carousel">Html</a></li>
+                                                    <li class="tab col s4 p-0"><a class="p-0" href="#js-carousel">JS</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div id="view-carousel">
+                                        <div class="row">
+                                            <div class="col s12">
+                                                <div class="carousel">
+                                                    <a class="carousel-item" href="#one!">
+                                                        <img src="../../../app-assets/images/gallery/1.png" alt="">
+                                                    </a>
+                                                    <a class="carousel-item" href="#two!">
+                                                        <img src="../../../app-assets/images/gallery/2.png" alt="">
+                                                    </a>
+                                                    <a class="carousel-item" href="#three!">
+                                                        <img src="../../../app-assets/images/gallery/3.png" alt="">
+                                                    </a>
+                                                    <a class="carousel-item" href="#four!">
+                                                        <img src="../../../app-assets/images/gallery/4.png" alt="">
+                                                    </a>
+                                                    <a class="carousel-item" href="#five!">
+                                                        <img src="../../../app-assets/images/gallery/5.png" alt="">
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div id="html-carousel">
+                                        <pre><code class="language-markup">
+&lt;div class="carousel">
+  &lt;a class="carousel-item" href="#one!">&lt;img src="https://lorempixel.com/250/250/nature/1">&lt;/a>
+  &lt;a class="carousel-item" href="#two!">&lt;img src="https://lorempixel.com/250/250/nature/2">&lt;/a>
+  &lt;a class="carousel-item" href="#three!">&lt;img src="https://lorempixel.com/250/250/nature/3">&lt;/a>
+  &lt;a class="carousel-item" href="#four!">&lt;img src="https://lorempixel.com/250/250/nature/4">&lt;/a>
+  &lt;a class="carousel-item" href="#five!">&lt;img src="https://lorempixel.com/250/250/nature/5">&lt;/a>
+&lt;/div>
+          </code></pre>
+                                    </div>
+                                    <div id="js-carousel">
+                                        <pre><code class="language-javascript">
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+    </code></pre>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+                </div>
+
+
+
+            </div>
+            <div class="content-overlay"></div>
+        </div>
+    </div>
+</div>
+<!-- END: Page Main-->
---
+++ app/Views/admin/layout/footer.php
@@ -0,0 +1,26 @@
+    <!-- BEGIN: Footer-->
+
+    <footer class="page-footer footer footer-static footer-dark gradient-45deg-indigo-purple gradient-shadow navbar-border navbar-shadow">
+        <div class="footer-copyright">
+            <div class="container"><span>&copy; 2020 <a href="http://themeforest.net/user/pixinvent/portfolio?ref=pixinvent" target="_blank">PIXINVENT</a> All rights reserved.</span><span class="right hide-on-small-only">Design and Developed by <a href="https://pixinvent.com/">PIXINVENT</a></span></div>
+        </div>
+    </footer>
+
+    <!-- END: Footer-->
+    <!-- BEGIN VENDOR JS-->
+    <script src="admin/js/vendors.min.js"></script>
+    <!-- BEGIN VENDOR JS-->
+    <!-- BEGIN PAGE VENDOR JS-->
+    <!-- END PAGE VENDOR JS-->
+    <!-- BEGIN THEME  JS-->
+    <script src="admin/js/plugins.js"></script>
+    <script src="admin/js/search.js"></script>
+    <script src="admin/js/custom/custom-script.js"></script>
+    <script src="admin/js/scripts/customizer.js"></script>
+    <!-- END THEME  JS-->
+    <!-- BEGIN PAGE LEVEL JS-->
+    <script src="admin/js/scripts/advance-ui-carousel.js"></script>
+    <!-- END PAGE LEVEL JS-->
+    </body>
+
+    </html>
